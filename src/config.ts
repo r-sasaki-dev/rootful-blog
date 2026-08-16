d9cf9e9147869e9f950e6a1b153030cc856deab0
ricosgit <sasakiriku36@gmail.com>
2026-08-16 16:34:49 +0900

    style: change blog title and basic info

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,11 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "ROOTFUL",
+	subtitle: "",
+	lang: "ja", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 90, // Brand color #A6F15D converted to hue (≈90.4°, rounded)
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
