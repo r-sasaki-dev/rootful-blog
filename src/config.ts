896052f711785e3cf61055b776049b107a83670c
ricosgit <sasakiriku36@gmail.com>
2026-08-16 16:30:58 +0900

    style: change favicon

--- src/config.ts
+++ src/config.ts
@@ -30,12 +30,22 @@ export const siteConfig: SiteConfig = {
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
-		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+			src: "/favicon/favicon-rootful-32.png",
+			sizes: "32x32",
+		},
+		{
+			src: "/favicon/favicon-rootful-128.png",
+			sizes: "128x128",
+		},
+		{
+			src: "/favicon/favicon-rootful-180.png",
+			sizes: "180x180",
+		},
+		{
+			src: "/favicon/favicon-rootful-192.png",
+			sizes: "192x192",
+		},
 	],
 };
 
